e40e15ff782b97d43e6f4c73a29370e870814eb2
Sannidhy Prabhakar <sanprabhkr2016@gmail.com>
2026-01-02 23:06:22 +0530

    minor fix

--- app/main.py
+++ app/main.py
@@ -826,9 +826,9 @@ def execute_single_command(connection, command, arguments, Database, stream_last
         #if server is master that is running on port 6379 then return role as  master else return role as slave
         server_port = connection.getsockname()[1]
         if server_port == 6379:
-            connection.sendall(b"role:master\r\n")
+            connection.sendall(b"+role:master\r\n")
         else:
-            connection.sendall(b"role:slave\r\n" + f"master_port:{server_port}\r\n")
+            connection.sendall(b"+role:slave\r\n" + f"master_port:{server_port}\r\n")
 
     else:
         connection.sendall(b"-ERR unknown command\r\n")
